6afed493ac529af5c99ab824d6d1f42de2e0b1e2
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-06-11 15:09:11 +0600

    added a navbar and a banner to the home page

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <img className='w-full rounded' src="https://d3nfwcxd527z59.cloudfront.net/content/uploads/2022/04/11084112/Gavi-Barcelona-Sevilla.jpg" alt="" />
+        </div>
+    );
+};
+
+export default Banner;
